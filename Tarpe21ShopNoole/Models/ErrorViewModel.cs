079444afe389f9db5af8b2bc726e14b4aaa1fa03
KaarelNoole <kaarelnoole@gmail.com>
2023-11-16 09:33:56 +0200

    Add project files.

---
+++ Tarpe21ShopNoole/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarpe21ShopNoole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
